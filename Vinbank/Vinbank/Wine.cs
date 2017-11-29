a9ad6070d78057a7a93afa96d9ab17fac5262272
Helge Stenström <h.stenstrom@gmail.com>
2017-11-29 21:26:22 +0100

    Klassdiagram påbörjat, med några metoder.

---
+++ Vinbank/Vinbank/Wine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vinbank
+{
+    public class Wine
+    {
+        public WineTypes WineType
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public CharacterTypes CharacterType
+        {
+            get => default(CharacterTypes);
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public DateTime DateAdded
+        {
+            get => default(DateTime);
+            set
+            {
+            }
+        }
+
+        public bool IsConsumed
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Vintage
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public void Consume()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Unconsume()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Vinbank/Vinbank/WineHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vinbank
+{
+    public class WineCellar
+    {
+        public List<Wine> WineList
+        {
+            get => default(List<Wine>);
+            set
+            {
+            }
+        }
+
+        /// <returns>A list of wines yet not consumed, in the wine cellar.</returns>
+        public Wine GetNonConsumed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Add a wine to the wine cellar
+        /// </summary>
+        public void Add(Wine wine)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Mark the wine pointed to by the index, as consumed.
+        /// </summary>
+        public void Consume(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
